c62a19da49b8ecdb5e878b14840f76b9b07570d8
JRBlockkop <161836282+JRBlockkop@users.noreply.github.com>
2026-04-15 21:34:37 +0200

    Initialize HTTP server with logging middleware

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+type Router struct{}
+
+func (rt *Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+
+	log.Printf("Method: %s | URL: %s | Header: %s\n", r.Method, r.URL.Path, r.Header)
+}
+
+func main() {
+	http.ListenAndServe(":3902", &Router{})
+}
